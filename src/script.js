07275e0e510cca11932150c0192348c9e75304d3
Tristan Peng <tp@gatech.edu>
2025-10-15 02:20:07 -0400

    use peerjs to send audio and transcript between host and client

--- src/script.js
+++ src/script.js
@@ -1,8 +1,31 @@
 import { Peer } from 'https://esm.sh/peerjs@1.5.5?bundle-deps';
+import vmsg from 'https://unpkg.com/vmsg@0.4.0/vmsg.js';
+
+const id = new URLSearchParams(window.location.search).get('id');
 
 const peer = new Peer();
+const open = new Promise(res => peer.on('open', () => res()));
+const conn = open.then(() => peer.connect(id));
+
+const questions = [];
+
+conn.then(c => {
+  c.on('open', () => {
+    c.on('data', data => {
+      switch (data.action) {
+        case 'question':
+          questions.push(data.payload);
+          break;
+      }
+    });
+  });
+});
+
+let transcript = '';
 
 const record = document.getElementById('record');
+const question = document.getElementById('question');
+const response = document.getElementById('response');
 
 const sr = window.SpeechRecognition || window.webkitSpeechRecognition;
 const recognition = new sr();
@@ -10,27 +33,35 @@ recognition.continuous = true;
 recognition.lang = 'en-US';
 recognition.interimResults = true;
 
-const stream = await navigator.mediaDevices.getUserMedia({ video: false, audio: true });
-
-peer.on('open', id => {
-  console.log('My peer ID is: ' + id);
-  const call = peer.call('7d823e44-3d33-41da-9f61-763becc2e776', stream);
-});
+const recorder = new vmsg.Recorder({ wasmURL: 'https://unpkg.com/vmsg@0.4.0/vmsg.wasm' });
+recorder.initAudio();
+recorder.initWorker();
 
 const start = async () => {
+  await recorder.initAudio();
+  await recorder.initWorker();
   recognition.start();
+  recorder.startRecording();
 };
 
-const end = () => {
+
+const end = async () => {
   recognition.stop();
+  (await conn).send({ action: 'transcript', payload: transcript });
+  const blob = await recorder.stopRecording();
+  (await conn).send({ action: 'recording', payload: blob });
+
+  const q = questions.shift();
+  question.innerText = q ? q : '';
 };
 
 record.addEventListener('mousedown', start);
 record.addEventListener('mouseup', end);
 
 record.addEventListener('touchstart', start);
-record.addEventListener('touchend', () => end);
+record.addEventListener('touchend', end);
 
 recognition.onresult = event => {
-  console.log(event.results);
+  transcript = event.results[0][0].transcript;
+  response.innerText = event.results[0][0].transcript;
 };
--- src/server/script.js
+++ src/server/script.js
@@ -1,11 +1,55 @@
 import { Peer } from 'https://esm.sh/peerjs@1.5.5?bundle-deps';
 
-const peer = new Peer();
+const connections = {};
+const connectionsObj = {};
+
+const smallest = (obj, excludeKey) => Object.entries(obj)
+  .filter(([key]) => key !== excludeKey)
+  .reduce((min, [key, value]) => min === null || value < obj[min] ? key : min, null);
+
+const peer = new Peer('92d7091d-6b23-4f18-8c2b-13cb98291a63123');
+// const peer = new Peer();
 
 peer.on('open', id => {
-  console.log('My peer ID is: ' + id);
+  new QRCode(document.getElementById('qr'), {
+    text: 'https://ccrma.stanford.edu/~pengt/friends?id=' + id,
+    colorDark: '#eee',
+    colorLight: 'transparent'
+  });
+  console.log('https://localhost:3000?id=' + id);
 });
 
-peer.on('call', call => {
-  console.log(call);
+peer.on('connection', conn => {
+  connections[conn.peer] = 0;
+  connectionsObj[conn.peer] = conn;
+
+  conn.on('open', () => {
+    conn.on('data', data => {
+      switch (data.action) {
+        case 'transcript':
+          const s = smallest(connections, conn.peer);
+          connections[s]++;
+          connectionsObj[s]?.send({ action: 'question', payload: data.payload });
+          break;
+        case 'recording':
+          const a = document.createElement('a');
+          a.style.display = 'none';
+          a.href = URL.createObjectURL(new Blob([data.payload], { type: 'audio/mp3' }));
+          a.download = new Date().getTime() + '.wav';
+          a.click();
+          setTimeout(() => {
+            URL.revokeObjectURL(a.href);
+          }, 100);
+          connections[conn.peer]--;
+          break;
+      }
+    });
+  });
 });
+
+// BUG: Not working
+peer.on('disconnected', conn => {
+  delete connections[conn.peer];
+  delete connectionsObj[conn.peer];
+});
+
